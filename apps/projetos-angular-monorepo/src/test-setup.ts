70e43bbc08e03566f4aa3dcffda7549c68e26727
wrferreira1003 <wrf.wellington@gmail.com>
2025-04-23 17:00:09 -0300

    Initial:create project

--- apps/projetos-angular-monorepo/src/test-setup.ts
+++ apps/projetos-angular-monorepo/src/test-setup.ts
@@ -1,5 +1,6 @@
 import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
 
+// Configuração do ambiente de teste
 setupZoneTestEnv({
   errorOnUnknownElements: true,
   errorOnUnknownProperties: true,
